5195c5dc118abbec838cfa75d331c829ef7155fc
c4yll1n <ceylinndmrkn@gmail.com>
2024-12-26 14:58:15 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            groupBox2.Visible = false;
+            groupBox3.Visible = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            groupBox2.Visible = true;
+            groupBox3.Visible = false;
+            groupBox2.Location = groupBox1.Location;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            groupBox2.Visible = false;
+            groupBox3.Visible = true;
+            groupBox3.Location = groupBox1.Location;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+         string sql="SELECT *FROM sistemodasi WHERE kullanici='"
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+        
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
